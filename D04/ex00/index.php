31da635ae7777df8b35561e6d1031eb96cfd2f51
Daniel COLOMA <dcoloma@e1r8p1.42.fr>
2019-05-24 23:20:50 +0200

    UPDATE ex01
    Signed-off-by: Daniel COLOMA <dcoloma@e1r8p1.42.fr>

---
+++ D04/ex00/index.php
@@ -0,0 +1,15 @@
+<?PHP
+session_start();
+if (isset($_GET["login"]) && isset($_GET["passwd"]) && isset($_GET["submit"])){
+    $_SESSION['login'] = $_GET["login"];
+    $_SESSION['passwd'] = $_GET["passwd"];
+}
+?>
+<html><body>
+<form action="index.php" method="get">
+    Identifiant: <input type="text" name="login" value="<?php echo $_SESSION['login']?>" />
+    <br />
+    Mot de passe: <input type="text" name="passwd" value="<?php echo $_SESSION['passwd']?>" />
+    <input type="submit" name="submit" value="OK">
+</form>
+</body></html>
