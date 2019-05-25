344b83115e515db52834b2d11a937a89d82aa5a2
Daniel COLOMA <dcoloma@e1r8p1.42.fr>
2019-05-25 16:23:21 +0200

    ADD ex03
    Signed-off-by: Daniel COLOMA <dcoloma@e1r8p1.42.fr>

---
+++ D04/ex03/auth.php
@@ -0,0 +1,15 @@
+<?PHP
+function auth($login, $passwd)
+{
+    $list = unserialize(file_get_contents("../private/passwd"));
+
+    foreach ($list as $tab => $user) {
+        $list_of_users[$user["login"]] = $user["passwd"];
+    }
+    foreach ($list_of_users as $login_registered => $passwd_registered){
+        if ($login_registered === $login && $passwd_registered === hash("md5", $passwd))
+            return (true);
+    }
+    return (false);
+}
+?>
